8956046cd7c080caf1f9ccf756958e48e2245737
9FS <pray4spam@googlemail.com>
2023-12-22 22:21:34 +0000

    added settings.json

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 # Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
+import json
+from KFSconfig import KFSconfig
 from KFSlog import KFSlog
 import logging
 import os
@@ -6,35 +8,45 @@ import os
 
 @KFSlog.timeit
 def main(DEBUG: bool) -> None:
-    DEST_PATH: str="./m3u/"                 # playlist file destination folder
-    LIBRARY_PATH: str="C:\\Users\\Felix\\Dropbox\\Music\\Library\\"
     MUSIC_FILE_EXT=(".m4a", ".mp3", ".wav") # music file extensions, recognise these files as music
     playlist_content: str                   # playlist current
     playlist_names: list
+    settings: dict[str, str]                # settings
+    SETTINGS_DEFAULT: str=json.dumps({      # settings default
+        "dest_path": "./m3u/",              # playlist file destination folder
+        "library_path": "",                 # library path absolute to prepend to every song name in playlist file
+    }, indent=4)
+
+
+    try:
+        settings=json.loads(KFSconfig.load_config("./config/settings.json",  SETTINGS_DEFAULT)) # load settings
+    except FileNotFoundError:
+        return
     
-    logging.info(f"Loading direct child directory names excluding \"{DEST_PATH}\" and \"./log/\"...")
+    
+    logging.info(f"Loading direct child directory names excluding \"{settings['dest_path']}\" and \"./log/\"...")
     playlist_names=[entry
                     for entry in os.listdir(".")
-                    if os.path.isdir(entry)==True and entry!="m3u" and entry!="log"]    # get all playlist folders, exclude destination folder if exists already, exclude Log folder
-    logging.info(f"\rLoaded direct child directory names excluding \"{DEST_PATH}\" and \"./log/\".")
+                    if os.path.isdir(entry)==True and entry!=settings["dest_path"].lstrip(".").strip("/") and entry!="log"] # get all playlist folders, exclude destination folder if exists already, exclude log folder
+    logging.info(f"\rLoaded direct child directory names excluding \"{settings['dest_path']}\" and \"./log/\".")
     logging.debug(playlist_names)
-    logging.debug(f"Creating destination directory \"{DEST_PATH}\"...")
-    os.makedirs(f"{DEST_PATH}", exist_ok=True)                                              # create destination folder
-    logging.debug(f"\rCreated destination directory \"{DEST_PATH}\".")
+    logging.debug(f"Creating destination directory \"{settings['dest_path']}\"...")
+    os.makedirs(f"{settings['dest_path']}", exist_ok=True)                                                                  # create destination folder
+    logging.debug(f"\rCreated destination directory \"{settings['dest_path']}\".")
     
-    logging.info("Creating playlist files...")
-    for playlist_name in playlist_names:                                # every playlist folder
+    logging.info("Creating playlist files...")          
+    for playlist_name in playlist_names:                                            # every playlist folder
         logging.info(f"Loading music files from \"{playlist_name}/\"...")
-        playlist_content="\n".join([os.path.join(LIBRARY_PATH, song)    # get every song in playlist folder, must be file and have music file extension, collapse list to string
-                                    for song in os.listdir(f"./{playlist_name}")
-                                    if os.path.isfile(song)==True and os.path.splitext(song)[1] in MUSIC_FILE_EXT])
+        playlist_content="\n".join([os.path.join(settings["library_path"], song)    # get every song in playlist folder, must be file and have music file extension, collapse list to string
+                                    for song in os.listdir(f"./{playlist_name}/")
+                                    if os.path.isfile(f"./{playlist_name}/{song}")==True and os.path.splitext(song)[1] in MUSIC_FILE_EXT])
         logging.info(f"Loaded music files from \"{playlist_name}/\".")
         logging.debug(playlist_content)
         
-        logging.info(f"Saving playlist \"{DEST_PATH}{playlist_name}.m3u\"...")
-        with open(f"{DEST_PATH}{playlist_name}.m3u", "wt", encoding="utf-8") as playlist_file:  # save playlist
+        logging.info(f"Saving playlist \"{settings['dest_path']}{playlist_name}.m3u\"...")
+        with open(f"{settings['dest_path']}{playlist_name}.m3u", "wt", encoding="utf-8") as playlist_file:  # save playlist
             playlist_file.write(playlist_content)
-        logging.info(f"\rSaved playlist \"{DEST_PATH}{playlist_name}.m3u\".")
+        logging.info(f"\rSaved playlist \"{settings['dest_path']}{playlist_name}.m3u\".")
     logging.info("Created playlist files.")
     
     return
